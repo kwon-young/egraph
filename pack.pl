da3c2c5f4b77f207b3110623e7d5d042fa5963aa
Kwon-Young Choi <kwon-young.choi@hotmail.fr>
2026-04-13 11:52:00 +0200

    feat: add pack metadata file for SWI-Prolog package

---
+++ pack.pl
@@ -0,0 +1,10 @@
+name(egraph).
+title('E-Graphs and Equality Saturation for SWI-Prolog').
+version('0.1.0').
+author('Kwon-Young Choi', 'kwon-young.choi@hotmail.fr').
+maintainer('Kwon-Young Choi', 'kwon-young.choi@hotmail.fr').
+packager('Kwon-Young Choi', 'kwon-young.choi@hotmail.fr').
+home('https://github.com/kwon-young/egraph').
+download('https://github.com/kwon-young/egraph/releases/*.zip').
+provides(egraph).
+requires(prolog >= '9.3.23').
